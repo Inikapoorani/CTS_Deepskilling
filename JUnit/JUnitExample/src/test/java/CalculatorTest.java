159f312e9d9b04797be2d44dd16ccaa2ac91a57b
Inika <inika.siva@gmail.com>
2026-07-01 23:35:18 +0530

    JUnit Ex

--- JUnit/JUnitExample/src/test/java/CalculatorTest.java
+++ JUnit/JUnitExample/src/test/java/CalculatorTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+
+        Calculator cal = new Calculator();
+
+        assertEquals(5, cal.add(2, 3));
+
+    }
 }
