d7bd349638e02acee7fad493466f4703d57b914e
Inika <inika.siva@gmail.com>
2026-07-01 23:46:03 +0530

    AAA

--- JUnit/JUnitExample/src/test/java/CalculatorTest.java
+++ JUnit/JUnitExample/src/test/java/CalculatorTest.java
@@ -1,16 +1,38 @@
 package org.example;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up...");
+        calculator = new Calculator();
+    }
+
     @Test
     public void testAddition() {
 
-        Calculator cal = new Calculator();
+        // Arrange
+        int a = 10;
+        int b = 20;
 
-        assertEquals(5, cal.add(2, 3));
+        // Act
+        int result = calculator.add(a, b);
 
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up...");
+        calculator = null;
     }
 }
