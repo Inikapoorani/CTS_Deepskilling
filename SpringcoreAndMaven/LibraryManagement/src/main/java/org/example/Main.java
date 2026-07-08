a22fe6c3dee16d61ea152bdaf8d5be2b1145b689
Inika <inika.siva@gmail.com>
2026-07-08 23:15:32 +0530

    configMaven

--- SpringcoreAndMaven/LibraryManagement/src/main/java/org/example/Main.java
+++ SpringcoreAndMaven/LibraryManagement/src/main/java/org/example/Main.java
@@ -5,10 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
-        BookService service =
-                context.getBean("bookService", BookService.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService service = context.getBean("bookService", BookService.class);
         service.addBook();
     }
 }
